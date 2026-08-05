6fd2e9a000cf900b809fef66a40e5fb22d13ca70
Sudip <sudyp07@gmail.com>
2026-08-05 15:23:47 +0545

    do some changes in modules main file

--- Phase 2/Lesson - 10 Modules/modules.py
+++ Phase 2/Lesson - 10 Modules/modules.py
@@ -4,6 +4,9 @@ like use 'import' to include a module (built in your own)
 useful to break up a large program reusable seprate files
 """
 
+# to get all info about the modules in python print this
+print(help('modules'))
+
 import math
 import math as m
 from math import pi  ## --> not recommended to use like it (use above 2 ways for better result and no errors)
